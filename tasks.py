ae1015630dedbfb9e111a2cab27d2ecac3b0f754
Erozinho <zenycsoficial@gmail.com>
2024-06-29 20:57:49 -0300

    🚧: starting the scrap with rpaframework

--- tasks.py
+++ tasks.py
@@ -1,22 +1,37 @@
 from RPA.Browser.Selenium import Selenium
 from RPA.Robocorp.WorkItems import WorkItems
+from robocorp.tasks import task
+from dotenv import load_dotenv
 from setup import payload
 
+load_dotenv()
 
 class Challenge:
     def __init__(self):
         self.browser = Selenium()
-        self.browser.set_browser_implicit_wait(15)
+        # self.browser.set_browser_implicit_wait(15)
 
     def close_browser(self) -> None:
         self.browser.close_browser()
 
+    def open_target(self, url: str) -> None:
+        self.browser.open_available_browser(url)
+        self.browser.maximize_browser_window()
+
     def main(self) -> None:
-        # run everything
-        inv = WorkItems()
-        inv.get_input_work_item()
+        try:
+            # run everything
+            inv = WorkItems()
+            inv.get_input_work_item()
+            url = inv.get_work_item_variable['url']
+            self.open_target(url=url)
+
+        finally:
+            # close webpage on the end of execution
+            pass
 
 
 if __name__ == "__main__":
+    @task
     scrap = Challenge()
     scrap.main()
